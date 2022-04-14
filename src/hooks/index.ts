319e2167c0a5dcd9782bd4d84e825f6b0f4901a6
Damien Butt <damien.butt@norgateav.com>
2022-04-14 21:33:07 +0100

    feat(hooks): add useCrestronPublishDigital

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,6 +2,7 @@ export * from "./useCrestronAnalog";
 export * from "./useCrestronDigital";
 export * from "./useCrestronSerial";
 export * from "./useCrestronPublishAnalog";
+export * from "./useCrestronPublishDigital";
 export * from "./useCrestronSubscribeAnalog";
 export * from "./useCrestronSubscribeDigital";
 export * from "./useCrestronSubscribeSerial";
---
+++ src/hooks/useCrestronPublishDigital/index.ts
@@ -0,0 +1,53 @@
+import { publishEvent } from "@crestron/ch5-crcomlib";
+import CrestronCH5 from "@norgate-av/crestron-ch5-helper";
+
+interface DigitalActions {
+    push: () => void;
+    release: () => void;
+    click: () => void;
+    hold: (duration: number, callback: () => void) => void;
+}
+
+export function useCrestronPublishDigital(
+    signalName: string,
+): [DigitalActions] {
+    const signalType = CrestronCH5.SignalType.Boolean;
+
+    const publish = (value: boolean) => {
+        publishEvent(signalType, signalName, value);
+    };
+
+    const push = () => {
+        publish(true);
+    };
+
+    const release = () => {
+        publish(false);
+    };
+
+    const click = () => {
+        publish(true);
+        publish(false);
+    };
+
+    const hold = (duration: number, callback: () => void) => {
+        publish(true);
+
+        setTimeout(() => {
+            publish(false);
+            callback();
+        }, duration);
+    };
+
+    return [
+        {
+            push,
+            release,
+            click,
+            hold,
+        },
+    ];
+}
+
+export default useCrestronPublishDigital;
+export const useCrestronPublishBoolean = useCrestronPublishDigital;
