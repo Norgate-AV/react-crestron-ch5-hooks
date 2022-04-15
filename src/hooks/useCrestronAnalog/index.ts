23b92cfe75774392622f358ddefaf9e6532bc137
Damien Butt <damien.butt@norgateav.com>
2022-04-15 14:42:24 +0100

    refactor(hooks): update useCrestronAnalog to reuse existing subscribe and publish hooks

--- src/hooks/useCrestronAnalog/index.ts
+++ src/hooks/useCrestronAnalog/index.ts
@@ -1,45 +1,15 @@
-import { useState, useRef, useEffect } from "react";
-import {
-    subscribeState,
-    unsubscribeState,
-    publishEvent,
-} from "@crestron/ch5-crcomlib";
-import CrestronCH5 from "@norgate-av/crestron-ch5-helper";
 import { AnalogAction, AnalogStateCallback } from "../../types";
+import { useCrestronPublishAnalog } from "../useCrestronPublishAnalog";
+import { useCrestronSubscribeAnalog } from "../useCrestronSubscribeAnalog";
 
 export function useCrestronAnalog(
     signalName: string,
     callback?: AnalogStateCallback,
 ): [number, AnalogAction] {
-    const signalType = CrestronCH5.SignalType.Number;
-    const [state, setState] = useState<number>(0);
-    const callbackRef = useRef<AnalogStateCallback | undefined>();
+    const [state] = useCrestronSubscribeAnalog(signalName, callback);
+    const [action] = useCrestronPublishAnalog(signalName);
 
-    useEffect(() => {
-        callbackRef.current = callback;
-    }, [callback]);
-
-    useEffect(() => {
-        const id = subscribeState(signalType, signalName, (value: number) => {
-            setState(value);
-
-            if (callbackRef.current) {
-                callbackRef.current(value);
-            }
-        });
-
-        return () => {
-            unsubscribeState(signalType, signalName, id);
-        };
-    }, [signalName]);
-
-    return [
-        state,
-        {
-            setValue: (value: number) =>
-                publishEvent(signalType, signalName, value),
-        },
-    ];
+    return [state, action];
 }
 
 export default useCrestronAnalog;
