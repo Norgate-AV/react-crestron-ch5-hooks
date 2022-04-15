ad464eb08c8537db186cb3d0b8581f3610f52542
Damien Butt <damien.butt@norgateav.com>
2022-04-15 13:34:16 +0100

    feat(types): add StateCallback types

---
+++ src/types/SerialStateCallback/index.ts
@@ -0,0 +1,1 @@
+export declare type SerialStateCallback = (value: string) => void;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,6 @@
 export * from "./AnalogAction";
+export * from "./AnalogStateCallback";
 export * from "./DigitalAction";
+export * from "./DigitalStateCallback";
 export * from "./SerialAction";
+export * from "./SerialStateCallback";
