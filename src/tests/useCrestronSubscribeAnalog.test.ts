f42cc34f2a4d241137cc2b2f929d406c62d49335
Damien Butt <damien.butt@norgateav.com>
2022-04-18 08:23:33 +0100

    test: update test

--- src/tests/useCrestronSubscribeAnalog.test.ts
+++ src/tests/useCrestronSubscribeAnalog.test.ts
@@ -1,39 +1,45 @@
-// import { renderHook, act } from "@testing-library/react-hooks";
 import { renderHook } from "@testing-library/react";
-// import CrComLib from "@crestron/ch5-crcomlib";
+import * as CrComLib from "@crestron/ch5-crcomlib";
+import CrestronCH5 from "@norgate-av/crestron-ch5-helper";
 import { useCrestronSubscribeAnalog } from "../hooks/useCrestronSubscribeAnalog";
 
-describe("useCrestronSubscribeAnalog", () => {
-    test("should not return null", () => {
-        const { result } = renderHook(() => useCrestronSubscribeAnalog("1"));
+const signalType = CrestronCH5.SignalType.Analog;
+const subscribeState = jest.spyOn(CrComLib, "subscribeState");
+// const unsubscribeState = jest.spyOn(CrComLib, "unsubscribeState");
+// type SubscribeStateCallback<T> = (value: T) => void;
 
-        // act(() => {
-        //     result.current.increment();
-        // });
-        // console.log(result.current);
+describe("useCrestronSubscribeAnalog()", () => {
+    beforeEach(() => {
+        subscribeState.mockClear();
+    });
+
+    it("should not return null", () => {
+        const { result } = renderHook(() => useCrestronSubscribeAnalog("1"));
 
         expect(result.current).not.toBe(null);
     });
 
-    test("should not return undefined", () => {
+    it("should not return undefined", () => {
         const { result } = renderHook(() => useCrestronSubscribeAnalog("1"));
-
-        // act(() => {
-        //     result.current.increment();
-        // });
-        // console.log(result.current);
 
         expect(result.current).not.toBe(undefined);
     });
 
-    test("should return number array", () => {
+    it("should return number array", () => {
         const { result } = renderHook(() => useCrestronSubscribeAnalog("1"));
 
-        // act(() => {
-        //     result.current.increment();
-        // });
-        // console.log(result.current);
+        expect(result.current).toEqual(expect.arrayContaining([0]));
+    });
+
+    it("should call subscribeState() once", () => {
+        const { result } = renderHook(() => useCrestronSubscribeAnalog("1"));
 
         expect(result.current).toEqual(expect.arrayContaining([0]));
+        expect(subscribeState).toHaveBeenCalledTimes(1);
+        expect(subscribeState).toHaveBeenCalledWith(
+            signalType,
+            "1",
+            expect.any(Function),
+        );
     });
 });
